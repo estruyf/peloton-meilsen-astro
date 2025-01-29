bb300de27bb5487e333a8e7222758884c12f0f71
Elio Struyf <elio@struyfconsulting.be>
2025-01-29 21:24:20 +0100

    Update site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#342c3b',
+				'primary-light': '#463d50',
+				'primary-dark': '#221d26',
+				accent: '#ddcca8',
+			},
+		},
 	},
 	plugins: [],
 }
